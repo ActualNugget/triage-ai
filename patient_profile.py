850f8ba2fa9bd60ba3d036b634eee766b550ffe7
Jthe-2003 <jwtsfsg@gmail.com>
2024-01-10 23:53:44 +0800

    random patient generator

---
+++ patient_profile.py
@@ -0,0 +1,53 @@
+import pandas as pd 
+from pathlib import Path
+from collections import namedtuple
+from random import randint
+
+#get data from excel file
+folder_path = Path(__file__).parent.resolve()
+input_conditions_path = Path(__file__).parent / "input_conditions.xlsx"
+pd.set_option('display.max_rows', None) 
+pd.set_option('display.max_columns', None)
+df = pd.read_excel(input_conditions_path)
+
+#turn each possible case into an object
+possible_case = namedtuple ("case", ["condition", "priority", "facilities", "in_or_out"])
+cases = []
+
+for index, row in df.iterrows():
+    row = df.iloc[index]
+    priority = int(row['Priority'])
+    condition = row['Examples']
+    facilities = list(row['Facilities'].split(", "))
+    in_or_out = int(row['In/outpatient'])
+    temp = possible_case(condition, priority, facilities, in_or_out)
+    cases.append(temp)
+
+#generate random patient cases
+N = len(cases)
+    
+def generate_random_patient(num_cases, cases_list):
+    patient_profile = {}
+    i = randint(0, num_cases-1)
+    case = cases_list[i]
+    if case.in_or_out == 2:
+        case.in_or_out = randint(0,1)
+        if (condition == "Cataracts" or condition == "Limb fractor/ dislocation") and in_or_out == 0:
+            case.facilities.remove("Operating Theatre")
+    if case.in_or_out == 0:
+        status = "Outpatient"
+    elif case.in_or_out == 1:
+        status = "Inpatient"
+    patient_profile.update({
+        'Condition' : case.condition,
+        'Priority' : case.priority,
+        'Facilities Required' : case.facilities,
+        'Inpatient/Outpatient' : status,
+    })
+    
+    return patient_profile
+
+
+random_patient = generate_random_patient(N, cases)
+
+print(random_patient)
